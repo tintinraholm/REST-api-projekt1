28dac386e06d3bb2f86536e7f37628e665a8e313
LinaRehnstrom <rehnstli@arcada.fi>
2025-09-19 12:40:50 +0300

    Docker & Prisma

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+
 const app = express()
+const prisma = new PrismaClient()
+
 const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send("It works!")
+})
 
 app.listen(PORT, () => {
     try {
