f549033298d1282e7b2aca9758d296c69efb8bbf
LinaRehnstrom <rehnstli@arcada.fi>
2025-09-24 15:25:44 +0300

    Delete and Post working

--- routes/notes.js
+++ routes/notes.js
@@ -27,6 +27,38 @@ router.get("/:id", async (req, res) => {
         res.status(500).send({msg: "Error, cannot get id"})
     }
 })
+
+router.post('/', async (req, res) => {
+
+    try {
+        const newNote = await prisma.note.create({
+            data: {
+                author_id: parseInt(req.body.author_id), 
+                note: req.body.text
+            }
+        })  
+        res.json({msg: "New note created", id: newNote.id})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({msg: "Error: POST failed"})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+      
+    try {
+        const deleteNote = await prisma.note.delete({
+            where: {
+                id: parseInt(req.params.id),
+            }
+        })  
+        res.status(200).send({msg: "Note deleted", deleteNote})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({msg: "Error: DELETE failed"})
+}
+})
+
 router.put('/:id', async (req, res) => {
     const id = parseInt(req.params.id)
     try {
