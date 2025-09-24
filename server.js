118d965443241af81432879dec2b0b3dd55f1350
Maria Packalén <packamar@arcada.fi>
2025-09-24 15:10:56 +0300

    Add GET, POST and DELETE endpoints

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const { PrismaClient } = require('@prisma/client')
+require('dotenv').config()
 
 const app = express()
 const prisma = new PrismaClient()
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send("It works!")
 })
+
+const notesRouter = require('./routes/notes')
+app.use('/notes', notesRouter)
 
 app.listen(PORT, () => {
     try {
