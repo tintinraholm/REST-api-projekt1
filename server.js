24982518645ca67411f22f70cb42bc7870e11662
LinaRehnstrom <rehnstli@arcada.fi>
2025-09-25 11:44:02 +0300

    cors working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { PrismaClient } = require('@prisma/client')
+const cors = require("cors")
 
 require('dotenv').config()
 const app = express()
@@ -7,9 +8,13 @@ const prisma = new PrismaClient()
 
 const PORT = process.env.PORT || 8070
 app.use(express.json())
+app.use(cors())
 
 const notesRouter = require('./routes/notes')
 app.use('/notes', notesRouter)
+
+const drawingsRouter = require('./routes/drawings')
+app.use('/drawings', drawingsRouter)
 
 app.get('/', (req, res) => {
     res.send("It works!")
