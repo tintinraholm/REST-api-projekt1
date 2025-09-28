eb50ec7bc15d31fc264893e88233d67e6f32e5f3
LinaRehnstrom <rehnstli@arcada.fi>
2025-09-28 22:58:01 +0300

    cors fix

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express()
 const PORT = process.env.PORT || 8070
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://people.arcada.fi/~rehnstli/post-it-projekt1/",
+  credentials: true
+}))
 
 const notesRouter = require('./src/routes/notes')
 app.use('/notes', notesRouter)
