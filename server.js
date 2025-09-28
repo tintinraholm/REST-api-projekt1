7a6af72533737f64b0b5515cd7e4c7f81742fee1
LinaRehnstrom <rehnstli@arcada.fi>
2025-09-28 23:12:15 +0300

    syntax fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8070
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://people.arcada.fi/~rehnstli/post-it-projekt1/",
+  origin: "https://people.arcada.fi/~rehnstli/post-it-projekt1",
   credentials: true
 }))
 
